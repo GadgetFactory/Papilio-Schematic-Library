ac76480061f32b78cceafeba9f0ca88e6d966d42
Jack Gassett <jack.gassett@gadgetfactory.net>
2013-11-21 14:33:11 -0700

    reset build directory.

--- Papilio_Schematic_Projects/Papilio_Schematic_Library_Base_Template/clean.bat
+++ Papilio_Schematic_Projects/Papilio_Schematic_Library_Base_Template/clean.bat
@@ -1,10 +1,4 @@
-REM Saves bit files first
-copy build_PapilioOne_500K\*.bit .\saved_PapilioOne_500K.bit
-copy build_PapilioPro_LX9\*.bit .\saved_PapilioPro_LX9.bit
-
 REM Deletes all unnecessary files
 @echo off
-REM for /f %%F in ('dir /b /a-d ^| findstr /vile ".sch .xise .bit .bat"') do del "%%F"
+for /f %%F in ('dir /b /a-d ^| findstr /vile ".sch .xise .bit .bat"') do del "%%F"
 for /f %%F in ('dir /b  ^| findstr /vile ".sch .xise .bit .bat"') do rmdir /q /s "%%F"
-mkdir build_PapilioOne_500K
-mkdir build_PapilioPro_LX9
