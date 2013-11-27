151a0b6bfb4cf035d03962e752371c19c7b46c82
Jack Gassett <jack.gassett@gadgetfactory.net>
2013-11-26 17:47:37 -0700

    Added ability to add individual Wings.

--- Papilio_Schematic_Projects/Papilio_Schematic_Library_Base_Template/clean.bat
+++ Papilio_Schematic_Projects/Papilio_Schematic_Library_Base_Template/clean.bat
@@ -1,4 +1,9 @@
+set projectname=Audio-MODFile-simple
+
 REM Deletes all unnecessary files
 @echo off
-for /f %%F in ('dir /b /a-d ^| findstr /vile ".sch .xise .bit .bat"') do del "%%F"
+for /f %%F in ('dir /b /a-d ^| findstr /vile ".sch .xise .bit .bat .sym .vhd"') do del "%%F"
 for /f %%F in ('dir /b  ^| findstr /vile "Sketch"') do rmdir /q /s "%%F"
+
+move papilio_soc_base_papilio_pro.bit %projectname%-papilio-pro.bit
+move papilio_soc_base_papilio_one.bit %projectname%-papilio-one.bit
