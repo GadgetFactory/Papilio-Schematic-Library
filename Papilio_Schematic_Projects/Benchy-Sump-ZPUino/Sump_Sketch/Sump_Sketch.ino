9693f83e4318404adefcec6ad3a57d418dd089c9
Jack Gassett <jack.gassett@gadgetfactory.net>
2013-12-18 21:37:34 -0700

    Sump Logic Analyzer

---
+++ Papilio_Schematic_Projects/Benchy-Sump-ZPUino/Sump_Sketch/Sump_Sketch.ino
@@ -0,0 +1,26 @@
+HardwareSerial mySerial1(5);
+
+void setup() {
+  // put your setup code here, to run once:
+//  delay(3000);  
+  Serial.begin(115200);
+  mySerial1.begin(115200); 
+  pinMode(WAL0, OUTPUT);
+ digitalWrite(WAL0, HIGH); 
+   pinMode(WAL2, OUTPUT);
+ digitalWrite(WAL2, HIGH); 
+   pinMode(WAL4, OUTPUT);
+ digitalWrite(WAL4, HIGH); 
+   pinMode(WAL6, OUTPUT);
+ digitalWrite(WAL6, HIGH); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  if (mySerial1.available()) {
+   Serial.write(mySerial1.read()); 
+  }
+  if (Serial.available()) {
+   mySerial1.write(Serial.read()); 
+  }  
+}
