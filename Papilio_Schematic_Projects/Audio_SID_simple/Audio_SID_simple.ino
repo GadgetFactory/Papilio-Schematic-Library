e8d894f2c860a85a1dc7c5240a977b676a4fe280
Jack Gassett <jack.gassett@gadgetfactory.net>
2014-01-08 12:52:33 -0700

    Updated description

--- Papilio_Schematic_Projects/Audio_SID_simple/Audio_SID_simple.ino
+++ Papilio_Schematic_Projects/Audio_SID_simple/Audio_SID_simple.ino
@@ -1,6 +1,38 @@
 /*
-  Gadget Factory
-  SID Audio Player Example
+ Gadget Factory
+ SID Audio Player Example
+ To learn more about the Papilio Schematic Library please visit http://learn.gadgetfactory.net
+ 
+ To use this sketch do the following steps:
+ 1)
+ Load the bit file that defines the circuit required by this sketch by clicking on the bit file link below that corresponds to the type of Papilio board you have. 
+ You should see the Papilio Loader pop up and program the circuit for this example to your Papilio Board. 
+ IMPORTANT NOTE: Please be sure that you are programming the bit file to SPI Flash. ZPUino will not work if written to FPGA only.
+ 
+ 2)
+ Make sure you have selected the correct ZPUino board type and com port, the Papilio always uses channel B of the FT2232 so it is usually the second com port you see.
+ The board type should be ZPUino Vanilla or Hyperion depending on what the schematic shows, or look at the Board Type section below.
+ Be sure to select the board type for the type of Papilio you have.
+ 
+ 3)
+ Make sure you have the required hardware attached to your Papilio, look at the schematic to see what is required. Or look at the Hardware section below.
+ You can open a PDF showing the circuit defined in the bit file by selecting the appropriate link to view the scheamtic for the type of Papilio board you have.
+
+ 4)
+ Run the sketch, it should communicate with the ZPUino included in the bit file you loaded earlier. 
+ 
+ If you want to modify the circuit then make sure you have Xilinx ISE Webpack installed and click on the appropriate link below to modify the schematic.
+ If you do not have Xilinx ISE Webpack installed visit this tutorial to learn how. http://gadgetfactory.net/learn/2013/10/23/install-xilinxs-free-ise-webpack/
+ 
+ If the links below do not work then then open the sketch directory from Sketch/Show Sketch Folder (Ctl-K) and manually access the files defined in the links below.
+
+ 
+ Papilio Pro
+   Click to program bit file: sketchdir://LX9/papilio_pro.bit
+   Click to view schematic:   sketchdir://schematic_papilio_pro.pdf
+   Click to modify schematic: sketchdir://PSL_Papilio_Pro_LX9.xise
+
+ Related library documentation:  
   
  SID libraries
  http://www.papilio.cc/index.php?n=Papilio.SID
@@ -8,10 +40,11 @@
  
  Hardware:
  Due to the size of the sid libraries this will only run on the Papilio Pro.
- This will work with any hardware with an audio jack: Audio Wing, LogicStart MegaWing, Arcade MegaWing, RetroCade MegaWing.
+ This sketch will work with any hardware with an audio jack: Audio Wing, LogicStart MegaWing, Arcade MegaWing, RetroCade MegaWing.
+ This example circuit is wired for an Audio Wing connected to CH, to use with any of the other hardware click the link to modify the schematic to adjust for your hardware.
  
- ZPUino Variant:
- Use the Papilio Schematic Library to create a ZPUino that contains the SID audio component.
+ Board Type:
+ ZPUino Vanilla Variant for Papilio Pro
 
  created 2013
  by Jack Gassett
